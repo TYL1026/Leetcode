3a414d83325ba8dd1decfd8774fa017ebda2843f
TonyL <65515982+TYL1026@users.noreply.github.com>
2021-07-05 23:15:43 -0700

    Update two_sum.java

--- Java/two_sum.java
+++ Java/two_sum.java
@@ -1,11 +1,13 @@
 class Solution {
 public int[] twoSum(int[] nums, int target) {
     Map<Integer, Integer> map = new HashMap<>();
+    // load in to the hashmap
     for (int i = 0; i < nums.length; i++) {
         map.put(nums[i], i);
     }
     for (int i = 0; i < nums.length; i++) {
         int complement = target - nums[i];
+        //map.get(complement) != i is used to prevent adding numbers in same position. For example [3,2,4] 6. Prevent the [0,0] return
         if (map.containsKey(complement) && map.get(complement) != i) {
             return new int[] { i, map.get(complement) };
         }
