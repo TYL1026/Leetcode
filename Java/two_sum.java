3d8044f4611479224dd12c82ae7347344b408911
TonyL <65515982+TYL1026@users.noreply.github.com>
2022-02-15 22:23:53 -0800

    Update two_sum.java

--- Java/two_sum.java
+++ Java/two_sum.java
@@ -1,17 +1,14 @@
 class Solution {
-public int[] twoSum(int[] nums, int target) {
-    Map<Integer, Integer> map = new HashMap<>();
-    // load in to the hashmap
-    for (int i = 0; i < nums.length; i++) {
-        map.put(nums[i], i);
-    }
-    for (int i = 0; i < nums.length; i++) {
-        int complement = target - nums[i];
-        //map.get(complement) != i is used to prevent adding numbers in same position. For example [3,2,4] 6. Prevent the [0,0] return
-        if (map.containsKey(complement) && map.get(complement) != i) {
-            return new int[] { i, map.get(complement) };
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
         }
+        // In case there is no solution, we'll just return null
+        return null;
     }
-    throw new IllegalArgumentException("No two sum solution");
-}
 }
